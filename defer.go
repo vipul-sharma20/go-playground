cadc28c5d2d3bc91ac917a4de6e4c1708fd941db
Vipul <vipul.sharma20@gmail.com>
2017-06-02 01:49:08 +0530

    add defer with return

--- defer.go
+++ defer.go
@@ -6,4 +6,15 @@ func main() {
 	defer fmt.Println("Hey there")
 
 	fmt.Println("I was first")
+
+    i := mythical()
+    fmt.Println("return: ", i)
+}
+
+func mythical() int {
+    var i int = 3
+    defer fmt.Println("defer: ", i)
+
+    i++
+    return i
 }
