633626d94a8510ded32844d7121f7d79c3423350
Vipul <vipul.sharma20@gmail.com>
2017-06-10 23:57:32 +0530

    vim-go changes

--- linkedlist.go
+++ linkedlist.go
@@ -5,32 +5,32 @@ import "fmt"
 var ptr *Node
 
 type Node struct {
-    next *Node
-    value int
+	next  *Node
+	value int
 }
 
 type LinkedList struct {
-    start *Node
+	start *Node
 }
 
 func addNode(val int) *Node {
-    return &Node{value: val}
+	return &Node{value: val}
 }
 
 func (ll *LinkedList) setNext(val int) {
-    if ll.start == nil {
-        node := addNode(val)
-        ll.start = node
-        ptr = node
-    } else {
-        fmt.Println(ll, ptr)
-        ptr.next = addNode(val)
-        ptr = ptr.next
-    }
+	if ll.start == nil {
+		node := addNode(val)
+		ll.start = node
+		ptr = node
+	} else {
+		fmt.Println(ll, ptr)
+		ptr.next = addNode(val)
+		ptr = ptr.next
+	}
 }
 
 func main() {
-    ll := new(LinkedList)
-    ll.setNext(10)
-    ll.setNext(20)
+	ll := new(LinkedList)
+	ll.setNext(10)
+	ll.setNext(20)
 }
